8401257b51957ed9d401bba6f11e0a78541bb165
Luciano Elias <eliasvonarx@gmail.com>
2026-03-10 11:18:21 -0300

    fix: root redirige a index.html estatico

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
+import { redirect } from 'next/navigation'
 export default function Page() {
-  return (
-    <iframe
-      src="/insightpilot.html"
-      style={{
-        width: "100vw",
-        height: "100vh",
-        border: "none",
-        display: "block",
-      }}
-    />
-  );
+  redirect('/index.html')
 }
